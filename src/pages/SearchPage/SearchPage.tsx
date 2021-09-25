4743e45e3e4e0b9adfd0c630b69ac85d1a0e866e
pixel-pixel <bartishco@gmail.com>
2021-09-25 19:07:00 +0300

    [EDITED] Main bar

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import SerialCard from "../../components/SerialCard";
 import { useFindSeries } from "../../hooks";
-import SeriesList from "../../loyauts/SeriesList";
+import SeriesList from "../../layouts/SeriesList";
 import "./SearchPage.scss"
 
 export const SearchPage: FC = () => {
