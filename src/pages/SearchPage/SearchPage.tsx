68842f194d6e5d5d78ad92e58dfe34614a161d77
pixel-pixel <bartishco@gmail.com>
2021-09-25 18:51:03 +0300

    [ADDED] Series list layout

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,21 +1,15 @@
 import React, { FC } from "react";
-
 import SerialCard from "../../components/SerialCard";
 import { useFindSeries } from "../../hooks";
+import SeriesList from "../../loyauts/SeriesList";
+import "./SearchPage.scss"
 
 export const SearchPage: FC = () => {
   const series = useFindSeries()
 
-  return (  
-    <>
-      {series.map(s => (
-        <SerialCard 
-          name={s.name}
-          image={s.image?.medium}
-          summary={s.summary}
-          genres={s.genres}
-          rating={s.rating.average} />
-      ))}
-    </>
+  return ( 
+    <div className="search-page">
+       <SeriesList />
+    </div> 
   )
 }
